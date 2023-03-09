6fa025a81a95ea3248f5af4befc397c223d87c98
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2023-03-09 10:46:10 -0300

    feat: use case get user profile

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,16 @@ import { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
     const user = {
       id: randomUUID(),
