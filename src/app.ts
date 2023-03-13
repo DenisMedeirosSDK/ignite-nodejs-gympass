d70bcb58bb3c346d938b22d6dd7978c4f3c40170
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2023-03-13 13:10:06 -0300

    feat: implement refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import { fastifyJwt } from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
+
 import { env } from './env'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
@@ -8,7 +10,18 @@ import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
-app.register(fastifyJwt, { secret: env.JWT_SECRET })
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
