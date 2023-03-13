f2000ae3d5116cc787f413c83bfc09350e9ea74f
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2023-03-13 13:47:51 -0300

    feat: RBAC

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,12 +1,21 @@
+import { prisma } from '@/lib/prima'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import supertest from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await supertest(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john_doe@example.com',
-    password: '123456',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john_doe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
+
   const authResponse = await supertest(app.server).post('/sessions').send({
     email: 'john_doe@example.com',
     password: '123456',
